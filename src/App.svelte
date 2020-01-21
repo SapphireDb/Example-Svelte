3654c7bec0dde974d4f83c9d25ae07f005726996
morrisjdev <morrisj@live.de>
2020-01-21 19:58:46 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { SapphireDb } from 'sapphiredb';
+
+	const db = new SapphireDb({
+		serverBaseUrl: 'localhost:5000',
+		useSsl: false,
+		apiKey: 'webapp',
+		apiSecret: 'pw1234',
+		connectionType: 'websocket'
+	});
+
+	let values = [];
+
+	db.collection('demo.entries').values().subscribe((valuesUpdate) => {
+		values = valuesUpdate;
+	});
+</script>
+
+{#each values as value}
+	<div>
+		Id: {value.id}, Content: {value.content}
+	</div>
+{/each}
